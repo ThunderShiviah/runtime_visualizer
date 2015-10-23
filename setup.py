00aa173632cb1a0e8e8d83661e693729fb3232d1
Thunder <thunder@uoregon.edu>
2015-10-22 22:40:42 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, find_packages
+
+setup(name='Runtime_visualizer',
+    version='0.1',
+    description='Visualize the time complexity of functions',
+    author='Thunder Shiviah',
+    author_email='thunder.shiviah@gmail.com',
+    url='https://github.com/ThunderShiviah/runtime_visualizer',
+    # packages=['distutils', 'distutils.command'],
+    packages=find_packages(), 
+     )
